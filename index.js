663d3039687f2a29cc3a0de1e7eef523bc6fc0e1
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-11-24 12:09:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+//declare a variable called companyName and set it equal to scuber
+
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
